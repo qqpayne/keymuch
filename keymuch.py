246c614c1fad8695384451ed4e5d8ab4743b10df
qqpayne <ivanakostelov@gmail.com>
2021-01-16 18:10:20 +0300

    Fixed bug

--- keymuch.py
+++ keymuch.py
@@ -7,32 +7,37 @@ from src.keyHandler import *
 from datetime import datetime
 
 def setHourlyTimer():
+    global saveCallback, renewalCallback
     # firstly, it check how much time left until next hour
     # and sets timer to commit save after this time
     now = datetime.now()
     nextHour = datetime(now.year, now.month, now.day, now.hour+1)
     waitFor = (nextHour-now).seconds+2 # just to be sure
-    Timer(waitFor, save).start()
+    print(waitFor)
+    saveCallback = Timer(waitFor, save)
+    saveCallback.start()
     # then, it sets itself to be executed again after this time 
-    Timer(waitFor+1, setHourlyTimer).start()
+    renewalCallback = Timer(waitFor+1, setHourlyTimer)
+    renewalCallback.start()
 
 def save():
     saver.addEntry(currentHour(-1), keeper.counter)
     keeper.counter = 0
 
 def main():
+    setHourlyTimer()
     global saver, keeper, handler
     saver = Saver(path)
     keeper = Keeper(saver.lastValue)
     handler = Handler(keeper)
-
-    setHourlyTimer()
 
 if __name__ == "__main__":
     path = 'logfile.csv'
 
     def receiveSignal(signalNumber, frame):
         saver.addEntry(currentHour(), keeper.counter)
+        saveCallback.cancel()
+        renewalCallback.cancel()
         exit()
 
     signal.signal(signal.SIGINT, receiveSignal)
